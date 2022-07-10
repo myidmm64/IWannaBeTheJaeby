b33ad596ed40f5797bb5108c5ecfdcb30e63f49b
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-07-10 14:43:13 +0900

    0710_맵 수정 프리팹 업로드

--- Assets/Script/MousePos.cs
+++ Assets/Script/MousePos.cs
@@ -8,11 +8,25 @@ public class MousePos : MonoBehaviour
     [DllImport("user32.dll")]
     public static extern bool SetCursorPos(int X, int Y);
 
+    [SerializeField]
+    private GameObject _target = null;
+    [SerializeField]
+    private float _speed = 2f;
+
     public GameObject somethingToTest;
 
     private void Start()
     {
+        //Vector3 tmpScreenPos = Camera.main.WorldToScreenPoint(somethingToTest.transform.position);
+        //SetCursorPos((int)tmpScreenPos.x, Screen.height - (int)tmpScreenPos.y);
+    }
+
+    private void Update()
+    {
         Vector3 tmpScreenPos = Camera.main.WorldToScreenPoint(somethingToTest.transform.position);
         SetCursorPos((int)tmpScreenPos.x, Screen.height - (int)tmpScreenPos.y);
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3((int)tmpScreenPos.x, Screen.height - (int)tmpScreenPos.y, 0f));
     }
+
+
 }
