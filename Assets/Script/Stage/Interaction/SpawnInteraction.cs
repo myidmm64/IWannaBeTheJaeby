22f2f08ed30bd25e7e59981a2b4fc0e220044648
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-08-26 23:23:40 +0900

    0826_문방구 진입 전 끝

--- Assets/Script/Stage/Interaction/SpawnInteraction.cs
+++ Assets/Script/Stage/Interaction/SpawnInteraction.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnInteraction : Interaction
 {
+    [SerializeField]
+    private SpawnObjectInfo[] _spawnObjectInfo = null;
+    private bool _isFirst = true;
+
     public override void DoEnterInteraction()
     {
+        SpawnObjects();
     }
 
     public override void DoExitInteraction()
@@ -16,4 +22,59 @@ public class SpawnInteraction : Interaction
     {
     }
 
+    private void OnEnable()
+    {
+        if(_isFirst)
+        {
+            _isFirst = false;
+            for(int i = 0; i < _spawnObjectInfo.Length; i++)
+            {
+                if (_spawnObjectInfo[i].obj != null)
+                {
+                    _spawnObjectInfo[i].originPos = _spawnObjectInfo[i].obj.transform.position;
+                }
+            }
+        }
+    }
+    private void OnDisable()
+    {
+
+        for (int i = 0; i < _spawnObjectInfo.Length; i++)
+        {
+            if (_spawnObjectInfo[i].obj != null)
+            {
+                _spawnObjectInfo[i].obj.transform.position = _spawnObjectInfo[i].originPos;
+                _spawnObjectInfo[i].obj.SetActive(false);
+            }
+        }
+    }
+
+    private void SpawnObjects()
+    {
+        for(int i = 0; i<_spawnObjectInfo.Length; i++)
+        {
+            if (_spawnObjectInfo[i].obj != null)
+            {
+                _spawnObjectInfo[i].obj.SetActive(true);
+            }
+
+            else if (_spawnObjectInfo[i].spawnObj != null)
+            {
+                for(int j = 0; j < _spawnObjectInfo[i].spawnCount; j++)
+                {
+                    Instantiate(_spawnObjectInfo[i].spawnObj, _spawnObjectInfo[i].trm);
+                }
+            }
+        }
+    }
+}
+[System.Serializable]
+public struct SpawnObjectInfo
+{
+    [HideInInspector]
+    public Vector3 originPos;
+    public GameObject obj;
+    public GameObject spawnObj;
+    public Transform trm;
+    public int spawnCount;
 }
